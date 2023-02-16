13a356d74d10cb1e10a024ed1a8fe8127453fc45
AndreySh <shingaryovan@gmail.com>
2023-02-16 22:54:57 +1200

    возведения числа А в целую стень B через рекурсию

---
+++ ex07/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу возведения числа А в целую стень B через рекурсию
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int Calc(int n, int m)
+{
+    if (m == 0)return 1;
+    else return Calc(n, m - 1) * n;
+}
+
+int N = ReadInt("Введите число N: ");
+int M = ReadInt("Введите степень: ");
+
+Console.WriteLine(Calc(N, M));
