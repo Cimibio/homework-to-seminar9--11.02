7ef753c595a07fab7e4426992bf79784944b1edc
AndreySh <shingaryovan@gmail.com>
2023-02-16 21:50:01 +1200

    Показать натуральные числа от 1 до N через рекурсию, N задано

---
+++ ex01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+void CalcNat(int arg, int n)
+{
+    if (arg > 0) 
+    {
+        Console.WriteLine(n);
+        n++;
+        CalcNat(arg - 1,n);
+    }
+}
+
+int N = ReadInt("Введите число N: ");
+
+CalcNat(N, 1);
