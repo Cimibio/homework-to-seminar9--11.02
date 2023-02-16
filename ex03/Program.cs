bb5623f2d4abacec2740276f8a6e1df3f6591c80
AndreySh <shingaryovan@gmail.com>
2023-02-16 21:59:03 +1200

    Показать натуральные числа от M до N через рекурсию, N и M заданы

---
+++ ex03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать натуральные числа от M до N через рекурсию, N и M заданы
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+void CalcNat(int arg, int n)
+{
+    if (arg > 0) 
+    {
+        Console.WriteLine(n);
+        n++;
+        CalcNat(arg - 1,n);
+    }
+}
+
+int N = ReadInt("Введите число N: ");
+int M = ReadInt("Введите число M: ");
+
+CalcNat(N - M + 1, M);
