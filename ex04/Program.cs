d4e41c1e0e9e8e4231ea06562094f182ec8bfb17
AndreySh <shingaryovan@gmail.com>
2023-02-16 22:30:32 +1200

    Найти сумму элементов от M до N через рекурсию, N и M заданы

---
+++ ex04/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти сумму элементов от M до N через рекурсию, N и M заданы
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int CalcNat(int arg, int n)
+{
+    if (arg > n) return 0;
+    else return arg + CalcNat(arg + 1, n);
+}
+
+int N = ReadInt("Введите число N: ");
+int M = ReadInt("Введите число M: ");
+
+Console.WriteLine(CalcNat(N, M));
