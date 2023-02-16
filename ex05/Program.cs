9875a9f9cac4eceaf7a12b438c4834cb6d45e2b9
AndreySh <shingaryovan@gmail.com>
2023-02-16 22:36:02 +1200

    Найти сумму цифр числа через рекурсию

---
+++ ex05/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму цифр числа через рекурсию
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int CalcSum(int arg)
+{
+    if (arg == 0) return 0;
+    else return arg % 10 + CalcSum(arg / 10);
+}
+
+int N = ReadInt("Введите число N: ");
+
+Console.WriteLine(CalcSum(N));
